a568d52325d89527a741ec3103fd08dd7c80b0c6
Oto Junior <otojunior@gmail.com>
2015-04-08 15:44:46 -0300

    Adição de Licença Apache 2.0

--- generate-weld.sh
+++ generate-weld.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+# 
+#  Copyright 2013 Oto Soares Coelho Junior
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
 # Shell script to help project creation with Maven.
 # Author: Oto Junior
 # Date: 31/12/2013
